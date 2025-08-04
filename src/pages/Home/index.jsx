3a493451657356ad8d977c789115bb77c11ff901
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-08-04 19:45:04 +0200

    feat: install React Router and set up initial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
+function Index() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -22,4 +19,4 @@ function App() {
   );
 }
 
-export default App;
+export default Index;
