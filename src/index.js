8dfee236bfbb11a0911a9c482584c7b00ca9207d
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-09-04 16:00:14 +0200

    feat: add Header component to index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/'
 import Survey from './pages/Survey/'
+import Header from './components/Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
