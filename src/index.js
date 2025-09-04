0fcdc558693d973d7985a9174d05efa4811e29d6
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-09-04 16:28:52 +0200

    feat: Add Freelances and Results components in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/'
 import Survey from './pages/Survey/'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
 
@@ -14,6 +16,8 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
