e1ae4ca16acaf159244711826af5fd7f9c410c93
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-09-09 16:10:21 +0200

    feat: Import Footerin index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Survey from './pages/Survey/'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Error from './components/Error'
 import createGlobalStyle from 'styled-components'
 
@@ -23,11 +24,12 @@ root.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-  <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
         <Route path="/results" element={<Results />} />
         <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 )
